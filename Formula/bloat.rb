17f3f82b4d195b2f18a1713c00dae248bb7020dc
Akhil Gautam <akhilgautam123@gmail.com>
2026-04-26 20:34:19 +0530

    bloat 0.2.1

--- Formula/bloat.rb
+++ Formula/bloat.rb
@@ -1,16 +1,16 @@
 class Bloat < Formula
   desc "htop-style disk storage analyzer and cleanup TUI for macOS"
   homepage "https://github.com/akhil-gautam/bloat"
-  version "0.2.0"
+  version "0.2.1"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/akhil-gautam/bloat/releases/download/v0.2.0/bloat-v0.2.0-macos-arm64.tar.gz"
-      sha256 "f4d21d042513f19790e9c9fd650ab5da3892acbfaf2952a86ba86c9ccb5612ab"
+      url "https://github.com/akhil-gautam/bloat/releases/download/v0.2.1/bloat-v0.2.1-macos-arm64.tar.gz"
+      sha256 "8aaa447d0ef8dcdbec153d72d90ff600ef6e17655d9c5d7d83946cf1510eefdb"
     else
-      url "https://github.com/akhil-gautam/bloat/releases/download/v0.2.0/bloat-v0.2.0-macos-x86_64.tar.gz"
-      sha256 "55a81c03c9d5f791f55c28a961a3f134968cd490d145c7048d0299357a59770f"
+      url "https://github.com/akhil-gautam/bloat/releases/download/v0.2.1/bloat-v0.2.1-macos-x86_64.tar.gz"
+      sha256 "0803d83cd81f0597a91a78f4de738a8fd5dc38c118ec0e2de89d568d11aab607"
     end
   end
 
